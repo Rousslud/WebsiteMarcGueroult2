71ed36f346d6e0d5cdd57c2185a495e706fcc4e6
rousslud <rousselot.ludovic@yahoo.fr>
2018-03-12 19:00:42 +0100

    Initialization

---
+++ js/carousel.js
@@ -0,0 +1,14 @@
+$('.carousel').slick({
+    dots:true,
+    slidesPerRow:3,
+    rows:2,
+    responsive:[
+        {
+            breqkpoint:478,
+            settings:{
+                slidesPerRow:1,
+                rows:1
+            }
+        }
+    ]
+});
